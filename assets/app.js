ce8f52dd2b4cf515fb220a0c21d67ec5f7d6e997
Brandon Shankar <bshankar12@comcast.net>
2019-11-02 22:51:54 -0700

    added link to css and edited html with search bar

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,15 @@
 //create variables for the program
-var apiKey = h10JPz5uC89880QCKzXN1dGLBdYd0SuW;
+var apiKey = 'h10JPz5uC89880QCKzXN1dGLBdYd0SuW';
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=kittens&limit=10&api_key=wslWpWhssAgYDK6zVXacBDsacT47flr4";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+
+  var gifs = response.data
+  for (var i =0; i < gifs.length; i++){
+    console.log(gifs[i].images.original.url);
+  }
+});

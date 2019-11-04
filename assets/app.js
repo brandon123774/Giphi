19f89756deffac396e089f3f7066c33f9d14184d
Brandon Shankar <bshankar12@comcast.net>
2019-11-03 21:14:16 -0800

    added buttons and js functions

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,8 @@ $(document).ready(function () {
     }))
 });
 
-// function getSearch() {
-//     $(""#");
-// }
+
+
 //Array of car makes
 var topics = ["Acura", "BMW", "Cheverolet", "Dodge", "Ford", "GMC", "Honda", "Jeep", "Hyundai", "Toyota", "Mercedes-Benz", "Mazda"];
 
@@ -25,19 +24,40 @@ for (var i = 0; i < topics.length; i++) {
     $("#buttons").append(topicsBtn);
 
 }
+//create event listener for buttons
+$("button").on("click", function () {
+    //create variables for the program that link to Giphy website database
+    var car = $(this).attr("buttons");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=10JPz5uC89880QCKzXN1dGLBdYd0SuW&limit=10";
+    //use ajax method to 'get' the request from the Giphy URL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        //data that is retrieved from Giphy 
+        .then(function (response) {
+            console.log(response);
+            //save this data into an array
+            var results = response.data;
+            //create a loop to get more results pertaining to search term
+            for (var i = 0; i < results.length; i++) {
+                //add in ratings to this
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    //create a place to hold the gifs that come up
+                    var gifHolder = $("<div>");
+                    var rating = results[i].rating;
+                    //store the new ratings above the gifs
+                    var pOne = $("<p>").text("Rating: " + rating);
+                    //need an image tag 
+                    var imageOne = $("<img>");
+                    imageOne.attr("src", results[i].images.fixed_height.url);
+                    //need to stick these images to the page 
+                    gifHolder.append(pOne);
+                    gifHolder.append(imageOne);
+                    // put these into the gif location on the page
+                    $("#gifs-here").prepend(gifHolder);
+                }
+            }
+        })
 
-//create variables for the program that link to Giphy website database
-var apiKey = 'h10JPz5uC89880QCKzXN1dGLBdYd0SuW';
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=kittens&limit=10&api_key=wslWpWhssAgYDK6zVXacBDsacT47flr4";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-
-    var gifs = response.data
-    for (var i = 0; i < gifs.length; i++) {
-        console.log(gifs[i].images.original.url);
-    }
 });
